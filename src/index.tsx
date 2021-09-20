1ed3eb688e65053bea4fb44ef421c9393df17373
Nadia <rebinnaf@yahoo.com>
2021-09-20 21:33:53 +0430

    add basic configs of apollo to test

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css'
-
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+const GITHUB_BASE_URL = 'https://eu-west-1.aws.realm.mongodb.com/api/client/v2.0/app/recipes-slphl/graphql'
+
+const httpLink = new HttpLink({
+  uri: GITHUB_BASE_URL,
+  credentials: 'include',
+  headers: {
+    apiKey: `ypy6OoZTy30O7H08D8Ip5tQM6gQXJ0IrZ6PJrLjxhb9CGuSKFRtSQuCE6Z1FmsOW`,
+  },
+})
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+    ,
   </React.StrictMode>,
   document.getElementById('root')
 )
--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -1,118 +1,95 @@
 import { Wrap } from '@chakra-ui/react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
 
 import RecipePreview from '../components/RecipePreview'
 import Card from '../components/ui/Card'
 
+const GET_RECIPES = gql`
+  {
+    recipe {
+      _id
+      cookingTime
+      createdAt
+      name
+      numberOfServings
+      preparationSteps
+    }
+  }
+`
 export default function Recipes() {
   return (
-    <Wrap spacing="30px" justify="center" align="center" overflow="hidden">
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <RecipePreview
-        {...{
-          _id: '1',
-          name: 'Pasta',
-          ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
-          preparationSteps: ['nadia1', 'nadia2'],
-          numberOfServings: 3,
-          cookingTime: 10,
-          createdAt: 1,
-        }}
-      />
-      <Card
-        title="+"
-        subtitle=""
-        description="Add new recipe"
-        category="New REcipe"
-        buttonContent="Add New Recipe!"
-        targetPath="create"
-      ></Card>
-    </Wrap>
+    <Query query={GET_RECIPES}>
+      {({ data }) => {
+        return (
+          <Wrap spacing="30px" justify="center" align="center" overflow="hidden">
+            <RecipePreview
+              {...{
+                _id: '1',
+                name: 'Pasta',
+                ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
+                preparationSteps: ['nadia1', 'nadia2'],
+                numberOfServings: 3,
+                cookingTime: 10,
+                createdAt: 1,
+              }}
+            />
+            ---{JSON.stringify(data)}---
+            <RecipePreview
+              {...{
+                _id: '1',
+                name: 'Pasta',
+                ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
+                preparationSteps: ['nadia1', 'nadia2'],
+                numberOfServings: 3,
+                cookingTime: 10,
+                createdAt: 1,
+              }}
+            />
+            <RecipePreview
+              {...{
+                _id: '1',
+                name: 'Pasta',
+                ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
+                preparationSteps: ['nadia1', 'nadia2'],
+                numberOfServings: 3,
+                cookingTime: 10,
+                createdAt: 1,
+              }}
+            />
+            <RecipePreview
+              {...{
+                _id: '1',
+                name: 'Pasta',
+                ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
+                preparationSteps: ['nadia1', 'nadia2'],
+                numberOfServings: 3,
+                cookingTime: 10,
+                createdAt: 1,
+              }}
+            />
+            <RecipePreview
+              {...{
+                _id: '1',
+                name: 'Pasta',
+                ingredients: [{ product: 'suger', quantity: 2, unit: 'kg' }],
+                preparationSteps: ['nadia1', 'nadia2'],
+                numberOfServings: 3,
+                cookingTime: 10,
+                createdAt: 1,
+              }}
+            />
+            <Card
+              title="+"
+              subtitle=""
+              description="Add new recipe"
+              category="New Recipe"
+              buttonContent="Add New Recipe!"
+              targetPath="/recipes/create"
+            ></Card>
+          </Wrap>
+        )
+      }}
+    </Query>
   )
 }
