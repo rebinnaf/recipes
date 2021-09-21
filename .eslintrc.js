df67c1bed2fb1f89a3e4c165ad19bb57f54d3d6f
Nadia <rebinnaf@yahoo.com>
2021-09-21 19:21:27 +0430

    fix no unused params error bug of eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,5 +38,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
+    'no-unused-vars': 'off',
   },
 }
